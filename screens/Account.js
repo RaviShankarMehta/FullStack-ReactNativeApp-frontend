2a4021703c2d66b3e8f8e06884e9ee668651c873
Ravi Shankar Mehta <nitravi.m@gmail.com>
2024-03-21 01:16:14 +0530

    Account Screen upate api integrated

--- screens/Account.js
+++ screens/Account.js
@@ -1,16 +1,94 @@
-import { View, Text, StyleSheet } from "react-native";
-import React, { useContext } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import React, { useContext, useState } from "react";
 import FooterMenu from "../components/Menus/FooterMenu";
 import { AuthContext } from "../context/authContext";
-
+import axios from "axios";
 const Account = () => {
-  const [state] = useContext(AuthContext);
-  console.log(state);
+  const [state, setState] = useContext(AuthContext);
+  const { user } = state;
+  const [name, setName] = useState(user?.name);
+  const [password, setPassword] = useState(user?.password);
+  const [email] = useState(user?.email);
+  const [loading, setLoading] = useState(false);
+
+  // update user data
+  const handleUpdate = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.put("/auth/update", {
+        name,
+        password,
+        email,
+      });
+      setLoading(false);
+      let UD = JSON.stringify(data);
+      setState({ ...state, user: UD?.updatedUser });
+      alert(data && data.message);
+    } catch (error) {
+      alert(error.response.data.message);
+      setLoading(false);
+      console.log(error);
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>Name:-{state?.user.name}</Text>
-      <Text>Email:-{state?.user.email}</Text>
-      <Text>Role:-{state?.user.role}</Text>
+      <ScrollView>
+        <View style={{ alignItems: "center" }}>
+          <Image
+            source={{
+              url: "https://avatars.githubusercontent.com/u/97671508?v=4",
+            }}
+            style={{ height: 200, width: 200, borderRadius: 100 }}
+          />
+        </View>
+        <Text style={styles.warningText}>
+          Currently you can only update your name and password*
+        </Text>
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputText}>Name</Text>
+          <TextInput
+            style={styles.inputBox}
+            value={name}
+            onChangeText={(text) => setName(text)}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputText}>Email</Text>
+          <TextInput style={styles.inputBox} value={email} editable={false} />
+        </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputText}>Password</Text>
+          <TextInput
+            style={styles.inputBox}
+            value={password}
+            secureTextEntry={true}
+            onChangeText={(text) => setPassword(text)}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputText}>Role</Text>
+          <TextInput
+            style={styles.inputBox}
+            value={state?.user.role}
+            editable={false}
+          />
+        </View>
+        <View style={{ alignItems: "center" }}>
+          <TouchableOpacity style={styles.updateBtn} onPress={handleUpdate}>
+            <Text style={styles.updateBtnText}>
+              {loading ? "Please Wait..." : "Update Profile"}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
       <View style={{ flex: 1, justifyContent: "flex-end" }}>
         <FooterMenu />
       </View>
@@ -23,6 +101,44 @@ const styles = StyleSheet.create({
     margin: 20,
     justifyContent: "space-between",
     marginTop: 50,
+  },
+  warningText: {
+    color: "red",
+    fontSize: 13,
+    textAlign: "center",
+  },
+  inputContainer: {
+    marginTop: 20,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  inputText: {
+    fontWeight: "bold",
+    width: 70,
+    color: "gray",
+  },
+  inputBox: {
+    width: 250,
+    backgroundColor: "#ffffff",
+    marginLeft: 10,
+    fontSize: 16,
+    paddingLeft: 20,
+    borderRadius: 5,
+  },
+  updateBtn: {
+    backgroundColor: "black",
+    color: "white",
+    height: 40,
+    width: 250,
+    borderRadius: 10,
+    marginTop: 30,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  updateBtnText: {
+    color: "#ffffff",
+    fontSize: 16,
   },
 });
 
